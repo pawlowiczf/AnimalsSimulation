daf4928d2e306f6a2a5185c7961cb16e29080582
Filip Pawłowicz <pawlowicz.filip137@gmail.com>
2023-10-19 11:14:16 +0200

    Created

--- AnimalsSimulation/src/main/java/agh/ics/oop/World.java
+++ AnimalsSimulation/src/main/java/agh/ics/oop/World.java
@@ -1,27 +1,33 @@
 package agh.ics.oop;
+import agh.ics.oop.model.MoveDirection;
+import static agh.ics.oop.OptionsParser.DirectionParser;
 
-import java.sql.SQLOutput;
 
 public class World {
     //
-    public static void run(String[] args) {
+    public static void run(MoveDirection[] args)
+    {
         //
         System.out.println("Start");
-        for(String x : args) {
+        for(MoveDirection x : args) {
             //
             switch(x) {
-                case "f" -> System.out.println("Zwierzak idzie do przodu");
-                case "b" -> System.out.println("Zwierzak idzie do tyłu");
-                case "r" -> System.out.println("Zwierzak skręca w prawo");
-                case "l" -> System.out.println("Zwierzak skręca w lewo");
+                case FORWARD  -> System.out.println( "Zwierzak idzie do przodu");
+                case BACKWARD -> System.out.println( "Zwierzak idzie do tyłu"  );
+                case RIGHT    -> System.out.println( "Zwierzak skręca w prawo" );
+                case LEFT     -> System.out.println( "Zwierzak skręca w lewo"  );
             }
         }
         System.out.println("Stop");
     }
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         //
         System.out.println("System wystartował");
-        run(args);
+
+        MoveDirection[] MoveDirection = DirectionParser(args);
+        run(MoveDirection);
+
         System.out.println("System zakończył działanie");
     }
 }
