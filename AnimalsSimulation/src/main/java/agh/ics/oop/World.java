7331034c181b48faa481c774784e02c997d8d088
Filip Pawlowicz <pawlowicz.filip137@gmail.com>
2023-10-31 14:11:26 +0100

    added unit tests to 'OptionParser' class added unit tests to 'MapDirection' class

--- AnimalsSimulation/src/main/java/agh/ics/oop/World.java
+++ AnimalsSimulation/src/main/java/agh/ics/oop/World.java
@@ -33,15 +33,5 @@ public class World {
 
         System.out.println("System zakończył działanie");
 
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
-
-        MapDirection dir = MapDirection.NORTH;
-        System.out.println( dir.previous() );
-        System.out.println( dir.next() );
-        System.out.println( dir.toString() );
     }
 }
--- AnimalsSimulation/src/main/java/agh/ics/oop/model/Vector2d.java
+++ AnimalsSimulation/src/main/java/agh/ics/oop/model/Vector2d.java
@@ -73,4 +73,6 @@ public class Vector2d {
     public int hashCode() {
         return Objects.hash(x, y);
     }
+
+
 }
