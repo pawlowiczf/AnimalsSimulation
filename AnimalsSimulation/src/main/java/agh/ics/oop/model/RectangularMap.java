591628c3a465738f9802e76db0dc50b10b7e675b
Filip Pawłowicz <pawlowicz.filip137@gmail.com>
2023-11-15 19:47:53 +0100

    commit lab, visualizer, map, rectangle

---
+++ AnimalsSimulation/src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -0,0 +1,12 @@
+package agh.ics.oop.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RectangularMap implements WorldMap {
+    //
+    private Map <Vector2d, Animal> animals = new HashMap<>();
+
+    boolean place
+
+}
