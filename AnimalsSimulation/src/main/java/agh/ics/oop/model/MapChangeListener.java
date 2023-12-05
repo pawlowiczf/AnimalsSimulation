eff2b1fb4110956320829f7475dd531778e9454b
Filip Pawlowicz <pawlowicz.filip137@gmail.com>
2023-12-05 22:16:03 +0100

    last corrections to 'MapChangeListener' class.

--- AnimalsSimulation/src/main/java/agh/ics/oop/model/MapChangeListener.java
+++ AnimalsSimulation/src/main/java/agh/ics/oop/model/MapChangeListener.java
@@ -4,4 +4,9 @@ public interface MapChangeListener {
     //
     void mapChanged(WorldMap worldMap, String message);
 
+    // Wzorzec projektowy - observer - stosowałbym go wtedy, gdy zmiana atrybutu, wywołanie jakiejś metody w danej klasie
+    // musi pociągać za sobą pewne zmiany w innych klasach. Możemy zastosować ten wzorzeć i jeśli coś zmieni
+    // się, to wszystkie klasy, które implementują ten interfejs, wykonają jakąś logikę. Jest to taki 'trigger',
+    // reakcja na zmianę. Tak to rozumiem :)
+
 }
